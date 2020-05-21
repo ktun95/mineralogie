6ddae9dab84844662339ef030229766fc3dc4496
Kevin Tun <kevintun01@gmail.com>
2020-05-21 12:42:47 -0500

    fixed PORT variable on server

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const session = require('express-session')
 const app = express()
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const db = require('./db/')
 
 app.use(express.static(path.join(__dirname, '..', '/public')))
