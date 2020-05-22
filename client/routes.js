80d7bb948d94daf62b688f6ceca5a4594e28b3f1
Kevin Tun <kevintun01@gmail.com>
2020-05-22 12:42:48 -0500

    added redirect from / to /browse

--- client/routes.js
+++ client/routes.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Redirect } from 'react-router-dom'
 import { BrowseProducts, SingleProduct, Login, Profile, About } from './components'
 
 export const Routes = () => {
     return (
         <div id="content-container">
             <div id="routes">
+                <Route exact path="/">
+                    <Redirect to="/browse" />
+                </Route>
                 <Route path="/products/:id" component={SingleProduct} />
                 <Route path="/products" component={BrowseProducts} />
                 <Route path="/login" component={Login} />
